93e3af1041d4801a3f04c43c6ba598509d7792a0
edwinbarrios81 <edwinbarrios81@gmail.com>
2021-04-28 11:11:26 -0500

    27-04-21 practica variables

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+
+
+
+
+// function saludar (string $p1 = " Edwin", int $p2 = 39){
+    
+//     return (string) "hola $p1 como estas tu edad es $p2<br>";
+        
+//         } 
+
+
+// echo saludar();
+// echo saludar("edwin", 23);
+// // echo saludar("Diana");
+// echo saludar("Jose");
+
+
+
+
+ 
+// function saludar($p1){
+    
+//     return (string) "hola $p1 como estas <br>";
+        
+//         } 
+
+
+// echo saludar("Edwin");
+// echo saludar("Diana");
+// echo saludar("Jose");
+
+// $nombre = (string) "Miguel";
+// echo $nombre;
+// echo "<br>";
+
+// $nombre = (string) "Edwin";
+// echo $nombre;
+// echo "<br>";
+
+// $nombre = (string) "Diana";
+// echo $nombre;
+// echo "<br>";
+
+// $nombre = (string) "William";
+// echo $nombre;
+// echo "<br>";
+
+
+
+
+
+// define("nombre", "Edwin Mauirio Barrios Almeida");
+// echo nombre;
+
+
+// $nombre = (string) "Edwin Mauricio Barrios Almeida";
+// $respuesta = settype($nombre, "int");
+// var_dump($respuesta);
+
+
+
+// $respuesta = define("CEDULA", 1234567, true);
+// echo CEDULA;
+// echo "<br>"
+// echo CEDULA; 
+
+
+
+
+
+//const cedula = 123;
+//echo cedula;
+
+//$nombre = (string) "Edwin MAuricio Barrios Almeida";
+//echo $nombre;
+
+//$vaso =(string)"agua";
+//echo $vaso;
+//echo "<br>";
+
+
+//$vaso = (string)"cafe";
+//echo $vaso;
+//echo "<br>";
+
+//$vaso = (int)"23";
+//echo $vaso;
+//echo "<br>";
+
+
+?>
